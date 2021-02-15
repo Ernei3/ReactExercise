d31a3eae79af369191e2266766a50ad740358a15
Ernei3 <45853073+Ernei3@users.noreply.github.com>
2021-02-15 11:23:57 +0100

    cleaning defaults and helloWorld

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
+
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
+import helloWorld from './Pages/helloWorld';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Edited</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="content">
+        <div className="menu">
+            <nav>
+            <Link to="/helloWorld"><Button>HelloWorld</Button></Link>
+            </nav>
+        </div>
+        <Route path="/helloWorld" component={helloWorld}/>
+      </div>
+    </Router>
   );
 }
 
